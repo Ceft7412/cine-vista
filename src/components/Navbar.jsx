7f9a5e5ae4b4fb0b783a62c23fa741b05308bc8f
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-18 18:10:49 +0800

    @updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,27 @@
 export default function Navbar() {
-  return <>
-    <nav>
-        
-    </nav>
-  
-  </>;
+  return (
+    <>
+      <nav className="navbar-top">
+        <div className="navbar-top__flex">
+          <div className="navbar-top__logo">
+            <h1>Logo</h1>
+          </div>
+          <div className="navbar-top__menu">
+            <span className="navbar-top-menu__item" title="Homepage">
+              Home
+            </span>
+            <span className="navbar-top-menu__item" title="Popular Movies">
+              Movies
+            </span>
+            <span className="navbar-top-menu__item" title="PTV Shows">
+              TV Shows
+            </span>
+            <span className="navbar-top-menu__item" title="Top IMDB">
+              Top IMDB
+            </span>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
