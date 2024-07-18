19e03c17864eef8f9c9ade0e28c4c0bfe5c5ed28
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-18 18:11:17 +0800

    @created landing page to hold component of navbar and landingcontent

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+
+/**
+ * components
+ */
+import Navbar from "../components/Navbar";
+import LandingContent from "../components/LandingContent";
+
+const Landing = () => {
+
+  return (
+    <>
+      <Navbar />
+      <LandingContent />
+    </>
+  );
+};
+
+export default Landing;
