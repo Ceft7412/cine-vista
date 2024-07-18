937d45f86992a3cb7f770ddfe3daa2cf48afa0a9
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-18 16:04:00 +0800

    @imported functions from react-router-dom

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+
+function App() {
+  return <></>;
+}
+
+export default App;
