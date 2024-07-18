6357c873658065c01ff29aa06bb8a902618527ad
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-19 06:21:00 +0800

    @updated function declaration

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,21 +1,17 @@
 import React from "react";
 
-
-
 /**
  * components
  */
 import Navbar from "../components/Navbar";
 import LandingContent from "../components/LandingContent";
 
-const Landing = () => {
-
+export default function Landing() {
   return (
     <>
       <Navbar />
       <LandingContent />
     </>
   );
-};
-
-export default Landing;
+}
+  
