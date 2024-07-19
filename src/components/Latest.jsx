e085f54744e6552cc18e607e50e72ca66d9f1a1b
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-19 22:51:15 +0800

    @added latest component layout

---
+++ src/components/Latest.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+
+export default function Latest() {
+  return (
+    <>
+      <div className="latest-block">
+        <div className="latest__flex">
+          <div className="latest__header"></div>
+          <div className="latest__content"></div>
+        </div>
+      </div>
+    </>
+  );
+}
