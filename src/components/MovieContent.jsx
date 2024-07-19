78c14f228f2ac305cb7a9bafd283fd98bc7c4d13
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-19 20:14:49 +0800

    @imported Trending

--- src/components/MovieContent.jsx
+++ src/components/MovieContent.jsx
@@ -4,15 +4,14 @@ import { useEffect, useState } from "react";
  * components
  */
 import Popular from "./Popular";
+import Trending from "./Trending";
 export default function MovieContent() {
-
-
   return (
     <>
       <div className="movie-content">
         <div className="movie-content__flex">
           <Popular />
-          <div className="trending__block"></div>
+          <Trending />
           <div className="latest-movies__block"></div>
         </div>
       </div>
