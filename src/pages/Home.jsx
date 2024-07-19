bec5c7926a68d8e4ea34a26636a9e0ab4c6c620d
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-19 16:42:19 +0800

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,13 @@
+/**
+ * components
+ */
+import Navbar from "../components/Navbar";
+import MovieContent from "../components/MovieContent";
 export default function Home() {
-  return <></>;
+  return (
+    <div className="root-container">
+      <Navbar showSearch={true} />
+      <MovieContent />
+    </div>
+  );
 }
