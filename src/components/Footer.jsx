3fe2f29b3cde764937917c6b29dea7ab051bd279
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-20 17:27:26 +0800

    @created footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <>
+      <footer className="footer">
+        <div className="footer__flex">
+          <span>&copy; {new Date().getFullYear()} Cedrick Caceres</span>
+        </div>
+      </footer>
+    </>
+  );
+}
