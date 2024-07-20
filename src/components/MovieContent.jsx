f41492dff8b1e8059f09685c92e35cfe2b10bf6e
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-20 17:27:33 +0800

    Update MovieContent.jsx

--- src/components/MovieContent.jsx
+++ src/components/MovieContent.jsx
@@ -7,13 +7,14 @@ import Popular from "./Popular";
 import Trending from "./Trending";
 import Latest from "./Latest";
 export default function MovieContent() {
+  const [hoveredId, setHoveredId] = useState(null);
   return (
     <>
       <div className="movie-content">
         <div className="movie-content__flex">
           <Popular />
           <Trending />
-          <Latest />
+          <Latest setHoveredId={setHoveredId} hoveredId={hoveredId} />
         </div>
       </div>
     </>
