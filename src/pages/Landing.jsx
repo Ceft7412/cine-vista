2c9f7e774144702aa875aaaa17cd2b5727d746cf
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-21 08:29:55 +0800

    Update Landing.jsx

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,16 +1,27 @@
-import React from "react";
+import { useState } from "react";
 
 /**
  * components
  */
 import Navbar from "../components/Navbar";
 import LandingContent from "../components/LandingContent";
+import MenuBlock from "../components/MenuBlock";
 
 export default function Landing() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="root-container">
-      <Navbar showSearch={false} />
-      <LandingContent />
-    </div>
+    <>
+      <div className="root-container">
+        <Navbar
+          showSearch={false}
+          bgColor={false}
+          setIsOpen={setIsOpen}
+          isOpen={isOpen}
+        />
+        <LandingContent />
+      </div>
+      <MenuBlock isOpen={isOpen} />
+    </>
   );
 }
