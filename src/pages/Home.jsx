425633c23fa2ecf2f10f6b4992eb1fcdc0419e08
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-21 08:29:53 +0800

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,19 @@
 import Navbar from "../components/Navbar";
 import MovieContent from "../components/MovieContent";
 import Footer from "../components/Footer";
+import { useState } from "react";
+import MenuBlock from "../components/MenuBlock";
+
 export default function Home() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="root-container">
-      <Navbar showSearch={true} />
-      <MovieContent />
-      <Footer />
-    </div>
+    <>
+      <div className="root-container">
+        <Navbar showSearch={true} bgColor={true} setIsOpen={setIsOpen} isOpen={isOpen} />
+        <MovieContent />
+        <Footer />
+      </div>
+      <MenuBlock isOpen={isOpen} />
+    </>
   );
 }
