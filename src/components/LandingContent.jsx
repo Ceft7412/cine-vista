67b15ec5a661a52435aecdfef13a05b9c82542b8
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-21 08:29:46 +0800

    Update LandingContent.jsx

--- src/components/LandingContent.jsx
+++ src/components/LandingContent.jsx
@@ -1,12 +1,29 @@
 import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
 import ArrowForwardRoundedIcon from "@mui/icons-material/ArrowForwardRounded";
-
+import { useRef, useContext } from "react";
 import { useNavigate } from "react-router";
+
+import { SearchContext } from "../App";
+
 export default function LandingContent() {
   const navigate = useNavigate();
+  const formRef = useRef(null);
 
+  const { searchTerm, setSearchTerm } = useContext(SearchContext);
+  console.log(searchTerm);
   const toHomepage = () => {
     navigate("home");
+  };
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
+    console.log(searchTerm);
+    if (searchTerm) {
+      navigate(`/search/${searchTerm}`);
+    }
   };
   return (
     <>
@@ -14,14 +31,17 @@ export default function LandingContent() {
       <div className="landing-content">
         <div className="landing-content__flex">
           <div className="landing-content__item" title="Search a movie, tv show and more">
-            <div className="landing-content__icon-wrapper">
+            <div className="landing-content__icon-wrapper" onClick={handleSearchSubmit}>
               <SearchRoundedIcon style={{ fontSize: 30 }} />
             </div>
-            <input
-              type="text"
-              className="landing-content__input"
-              placeholder="Find movie, tv show and more"
-            />
+            <form ref={formRef} onSubmit={handleSearchSubmit}>
+              <input
+                type="text"
+                className="landing-content__input"
+                placeholder="Find movie, tv show and more"
+                onChange={handleSearchChange}
+              />
+            </form>
           </div>
           <div
             className="landing-content__item button-redirect-landing"
